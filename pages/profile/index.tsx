0324878a29e9c0f60f9093a0e8de2ec96a4967b4
XIE CHEN <xiechen0692@gmail.com>
2023-07-06 15:16:21 -0600

    add nav header

---
+++ pages/profile/index.tsx
@@ -0,0 +1,48 @@
+import Header from "@/components/Header"
+import { motion } from "framer-motion"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Header></Header>
+      <p className="mb-10 dark:bg-black dark:text-white">fdsfsdfds</p>
+      <p className="mt-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+      <p className="mb-10">fdsfsdfds</p>
+    </main>
+  )
+}
