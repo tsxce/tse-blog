e8bebba9e182917e5301d9ad181fd4617d6ce136
XIE CHEN <xiechen0692@gmail.com>
2023-08-20 21:55:06 -0600

    finish stepper except animation

--- components/SideStepper/index.tsx
+++ components/SideStepper/index.tsx
@@ -1,42 +1,47 @@
 'use client'
 import useWindownPosition from '@/hook/useWindowPosition'
-import { useEffect, useRef, useCallback, useMemo } from 'react'
+import { useEffect, useRef, useCallback } from 'react'
 
 export default function SideStepper() {
   const { scrollPosition, totalHeight, viewHeight } = useWindownPosition()
-  const ref1 = useRef<HTMLDivElement>(null)
-  const ref2 = useRef<HTMLDivElement>(null)
-  const ref3 = useRef<HTMLDivElement>(null)
-  const refArray = useMemo(() => [ref1, ref2, ref3], [])
+  const screenNum = totalHeight / viewHeight
+  const list = [...Array(screenNum).keys()]
+  const refArray = useRef([])
 
   const addIndexColor = useCallback(
     (p) => {
-      refArray.forEach((refItem, index) => {
+      refArray.current.forEach((refItem, index) => {
         if (index === p) {
-          refItem.current?.classList.add('text-purple')
+          refItem.classList.add('fill-purple')
         } else {
-          refItem.current?.classList.remove('text-purple')
+          refItem.classList.remove('fill-purple')
         }
       })
     },
     [refArray],
   )
-
   useEffect(() => {
     const colorIndex = Math.ceil(scrollPosition / viewHeight)
     addIndexColor(colorIndex)
   }, [scrollPosition, viewHeight, addIndexColor])
   return (
-    <div className="fixed right-0 top-1/2 z-10 rotate-90 transform -translate-y-1/2 w-[100px] h-[20px]">
-      <div id={'stepper-1'} ref={ref1}>
-        *
-      </div>
-      <div id={'stepper-2'} ref={ref2}>
-        *
-      </div>
-      <div id={'stepper-3'} ref={ref3}>
-        *
-      </div>
+    <div className="fixed flex right-0 top-1/2 z-10 rotate-90 transform -translate-y-1/2 w-[100px] h-[20px]">
+      {list.map((x) => {
+        return (
+          <svg
+            key={x}
+            id={`stepper-${x}`}
+            className="ml-2"
+            ref={(el) => {
+              refArray.current[x] = el
+            }}
+            width="20"
+            height="6"
+          >
+            <rect width="300" height="100" />
+          </svg>
+        )
+      })}
     </div>
   )
 }
