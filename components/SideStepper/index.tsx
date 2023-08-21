4fea5b0f504f6a0eb4ac7e88dc9c834b1bb02afe
XIE CHEN <xiechen0692@gmail.com>
2023-08-20 19:04:26 -0600

    finish stepper logic

--- components/SideStepper/index.tsx
+++ components/SideStepper/index.tsx
@@ -1,32 +1,42 @@
 'use client'
 import useWindownPosition from '@/hook/useWindowPosition'
-import { useEffect } from 'react'
+import { useEffect, useRef, useCallback, useMemo } from 'react'
 
 export default function SideStepper() {
   const { scrollPosition, totalHeight, viewHeight } = useWindownPosition()
-  const result: any[] = []
-  for (let i = 0; i < totalHeight / viewHeight; i++) {
-    result.push(<div id={`stepper-${i + 1}`}>*</div>)
-  }
+  const ref1 = useRef<HTMLDivElement>(null)
+  const ref2 = useRef<HTMLDivElement>(null)
+  const ref3 = useRef<HTMLDivElement>(null)
+  const refArray = useMemo(() => [ref1, ref2, ref3], [])
+
+  const addIndexColor = useCallback(
+    (p) => {
+      refArray.forEach((refItem, index) => {
+        if (index === p) {
+          refItem.current?.classList.add('text-purple')
+        } else {
+          refItem.current?.classList.remove('text-purple')
+        }
+      })
+    },
+    [refArray],
+  )
+
   useEffect(() => {
-    // const steperId = 'stepper-' + (Math.ceil(scrollPosition / viewHeight) + 1)
-    // const stepperMaybeNullElement = window.document.getElementsByClassName('stepper-1')
-    const stepperMaybeNullElement =
-      document.querySelector<HTMLElement>(`#stepper-1`)!
-    // stepperMaybeNullElement.style.color = 'red'
-    stepperMaybeNullElement.classList.add('text-bg-purple')
-    console.log(stepperMaybeNullElement)
-    // if ( stepperMaybeNullElement === null ) {
-    //   alert('oops');
-    // } else {
-    //   stepperMaybeNullElement.style.color ='red'
-    // }
-    console.log(Math.ceil(scrollPosition / viewHeight) + 1)
-  }, [scrollPosition, viewHeight])
+    const colorIndex = Math.ceil(scrollPosition / viewHeight)
+    addIndexColor(colorIndex)
+  }, [scrollPosition, viewHeight, addIndexColor])
   return (
     <div className="fixed right-0 top-1/2 z-10 rotate-90 transform -translate-y-1/2 w-[100px] h-[20px]">
-      {/* {(scrollPosition + 1000) / totalHeight} */}
-      {result}
+      <div id={'stepper-1'} ref={ref1}>
+        *
+      </div>
+      <div id={'stepper-2'} ref={ref2}>
+        *
+      </div>
+      <div id={'stepper-3'} ref={ref3}>
+        *
+      </div>
     </div>
   )
 }
