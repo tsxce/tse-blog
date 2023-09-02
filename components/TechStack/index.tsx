f1e78d2ad41329c17daceb29eb1182e7f2c01b42
XIE CHEN <xiechen0692@gmail.com>
2023-09-01 23:46:45 -0600

    update

--- components/TechStack/index.tsx
+++ components/TechStack/index.tsx
@@ -2,18 +2,23 @@ import Image from 'next/image'
 import techImages from '@/utils/techImageImport'
 
 export default function TechStack() {
+  console.log(techImages)
   return (
-    <div className='flex w-[200px]'>
-        {
-            Object.keys(techImages).map((image, i) => {
-                console.log(techImages[image])
-                return (
-                    // <div className='w-2 h-2' key={i + 'image'}>
-                        <Image key={i} className='ml-3' width={300} height={300} src={techImages[image]['default']['src']} alt="" />
-                    // </div>
-                )
-            })
-        }
+    <div className="flex w-[200px]">
+      {Object.keys(techImages).map((image, i) => {
+        return (
+          // <div className='w-2 h-2' key={i + 'image'}>
+          <Image
+            key={i}
+            className="ml-3"
+            width={14}
+            height={12}
+            src={techImages[image]['default']['src']}
+            alt=""
+          />
+          // </div>
+        )
+      })}
     </div>
   )
 }
--- utils/techImageImport.ts
+++ utils/techImageImport.ts
@@ -1,11 +1,17 @@
-const importAll = require =>
-  require.keys().reduce((acc, next) => {
-    acc[next.replace("./", "")] = require(next);
-    return acc;
-  }, {});
+function importAll(r) {
+  //   let images = {}
+  //   r.keys().forEach((item, index) => {
+  //     images[item.replace('./', '')] = r(item)
+  //   })
+  let images = []
+  r.keys().forEach((item, index) => {
+    images.push(r(item))
+  })
+  return images.slice(0, Math.ceil(images.length / 2))
+}
 
 const techImages = importAll(
-  require.context("../images/tech-logos", false, /\.(png|jpe?g|svg)$/)
-);
+  require.context('../images/tech-logos/', false, /\.(png|jpe?g|svg)$/),
+)
 
-export default techImages;
+export default techImages
